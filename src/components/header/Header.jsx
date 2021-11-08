1cb689441a095ec7f06a473bf4512d5a08f08e10
Franco Ortega <francopdx@gmail.com>
2021-11-07 18:16:31 -0800

    add Star Wars font

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,7 @@ import styles from './Header.module.css';
 const Header = () => {
   return (
     <header className={styles.Header}>
-      <h1>STAR WARS <br /> Characters</h1>
+      <h1>STAR WARS Characters</h1>
     </header>
   );
 };
