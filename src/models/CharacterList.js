75df9b025e0cc2149568409d66ad5667f4f17fc9
Franco Ortega <francopdx@gmail.com>
2021-11-07 23:01:03 -0800

    render Averages in App

--- src/models/CharacterList.js
+++ src/models/CharacterList.js
@@ -5,24 +5,24 @@ export class CharacterList {
     this.next = next;
     this.previous = previous;
     this.characters = results
-    .map(character => {
-      return {
-        name: character.name,
-        gender: character.gender,
-        hairColor: character.hair_color,
-        mass: character.mass,
-        height: character.height
-      };
-    })
-    .sort((a, b) => {
-      const nameA = a.name.toUpperCase();
-      const nameB = b.name.toUpperCase();
+      .map(character => {
+        return {
+          name: character.name,
+          gender: character.gender,
+          hairColor: character.hair_color,
+          mass: character.mass,
+          height: character.height
+        };
+      })
+      .sort((a, b) => {
+        const nameA = a.name.toUpperCase();
+        const nameB = b.name.toUpperCase();
   
-      if(nameA < nameB) return -1;
-      if(nameA > nameB) return 1;
+        if(nameA < nameB) return -1;
+        if(nameA > nameB) return 1;
   
-      return 0;
-    });
+        return 0;
+      });
   }
 
   averageMass() {
