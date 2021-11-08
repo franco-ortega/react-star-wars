dea823b30e973b73727dfced4a76642fea850b56
Franco Ortega <francopdx@gmail.com>
2021-11-07 20:28:14 -0800

    add CharacterList component

--- src/components/characters/CharacterList.jsx
+++ src/components/characters/CharacterList.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const CharacterList = props => {
+const CharacterList = ({ characters }) => {
+  const charactersToDisplay = characters.map((character, index) => (
+    <Character
+      key={index}
+      name={character.name}
+      gender={character.gender}
+      hairColor={character.hairColor}
+      mass={character.mass}
+      height={character.height}
+    />
+  ));
+
   return (
-    <div>
-      Character List
-    </div>
+    <main>
+      {charactersToDisplay}
+    </main>
   );
 };
 
 CharacterList.propTypes = {
-
+  characters: PropTypes.array.isRequired
 };
 
 export default CharacterList;
