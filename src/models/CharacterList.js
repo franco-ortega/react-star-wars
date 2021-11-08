d33d8e284f4f7987caaa193377f0f2efcc053db4
Franco Ortega <francopdx@gmail.com>
2021-11-08 01:06:01 -0800

    add conditional statements to ensure that all numbers and only numbers are calculated for the averages

--- src/models/CharacterList.js
+++ src/models/CharacterList.js
@@ -26,15 +26,50 @@ export class CharacterList {
   }
 
   averageMass() {
-    return this.characters.reduce((acc, cur) => {
+    let totalCharacters = this.characters.length;
+
+    const average = this.characters.reduce((acc, cur) => {
+      // If the mass has a comma, remove the comma
+      if(cur.mass.includes(',')) {
+        const commaRemoved = cur.mass.split(',').join('');
+
+        // If the mass converted to a number is not NaN, add it to the total
+        if(!isNaN(Number(commaRemoved))) {
+          return acc + Number(commaRemoved)
+        }
+
+        // Otherwise, decrement the totalCharacters and only return the previous total
+        totalCharacters--;
+        return acc;
+      }
+
+      if(isNaN(Number(cur.mass))) {
+        totalCharacters--;
+        return acc;
+      }
+
       return acc + Number(cur.mass);
-    }, 0) / this.characters.length;
+    }, 0) / totalCharacters;
+
+    // If there is a decimal place longer than 2 digits, only return 2 digits
+    if(String(average).split('.')[1]) {
+      if(String(average).split('.')[1].length > 2) return average.toFixed(2);
+    }
+
+    return average;
   }
 
   averageHeight() {
+    let totalCharacters = this.characters.length;
+
     return this.characters.reduce((acc, cur) => {
+      if(isNaN(Number(cur.height))) {
+        totalCharacters--;
+        return acc;
+      }
+
       return acc + Number(cur.height);
-    }, 0) / this.characters.length;
+    }, 0) / totalCharacters;
   }
 
   totalPages() {
