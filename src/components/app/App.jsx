4762d0de7c1943af6661b541eb7b6bc3991572ea
Franco Ortega <francopdx@gmail.com>
2021-11-07 21:06:40 -0800

    fetch and display characters

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+
 import { getCharacters } from '../../services/getCharacters';
+import CharacterList from '../characters/CharacterList';
 import Header from '../header/Header';
 import Loading from '../loading/Loading';
 
@@ -12,10 +14,24 @@ const App = () => {
     characters: []
   });
 
+  useEffect(() => {
+    setLoading(true);
+
+    getCharacters(1)
+      .then(res => {
+        console.log(res);
+        setCharacters(res);
+        setLoading(false);
+      });
+  }, []);
+
+  console.log(characters);
+
   return (
     <div data-testid="app">
       <Header />
       {loading && <Loading />}
+      <CharacterList characters={characters.characters} />
     </div>
   );
 };
