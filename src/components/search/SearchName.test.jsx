58821a0d70928f120d4ae994c335407b5652b4c9
Franco Ortega <francopdx@gmail.com>
2021-11-08 13:37:36 -0800

    add missing prop to SearchName test

--- src/components/search/SearchName.test.jsx
+++ src/components/search/SearchName.test.jsx
@@ -6,6 +6,7 @@ describe('SearchName component tests', () => {
   it('renders SearchName component', () => {
     render(
       <SearchName
+      searchTerm={''}
       setSearchTerm={() => {}}
       setCurrentPage={() => {}}
       />
@@ -21,6 +22,7 @@ describe('SearchName component tests', () => {
 
     render(
       <SearchName
+      searchTerm={''}
         setSearchTerm={setSearchTerm}
         setCurrentPage={() => {}}
       />
